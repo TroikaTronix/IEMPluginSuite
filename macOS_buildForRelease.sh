12221c50fe96ac7ad6ce3e3e0ca5842f50e1fc1f
Felix Holzmüller <f.holzmueller@kug.ac.at>
2024-11-08 15:00:56 +0000

    Resolve "Graphic problem with AllRADecoder & JUCE 8.0.2"

--- macOS_buildForRelease.sh
+++ macOS_buildForRelease.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 ## script for creating the release binaries on
-rm -rf build
+# rm -rf build
 rm -rf VST
-# rm -rf VST3
+rm -rf VST3
 rm -rf LV2
+rm -rf AAX
 rm -rf Standalone
 
 mkdir build
@@ -12,12 +13,14 @@ cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DIEM_BUILD_VST2=ON -DVST2SDKPATH=src/VST_SDK/VST2_SDK/ -DIEM_BUILD_LV2=ON -DIEM_BUILD_STANDALONE=ON -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64"
 cmake --build .
 
-cd ..
+# cd ..
 mkdir VST
 cp -r build/*/*_artefacts/Release/VST/*.vst VST/
-# mkdir VST3
-# cp -r build/*/*_artefacts/Release/VST3/*.vst3 VST3/
+mkdir VST3
+cp -r build/*/*_artefacts/Release/VST3/*.vst3 VST3/
 mkdir LV2
 cp -r build/*/*_artefacts/Release/LV2/*.lv2 LV2/
+mkdir AAX
+cp -r build/*/*_artefacts/Release/AAX/*.aaxplugin AAX/
 mkdir Standalone
 cp -r build/*/*_artefacts/Release/Standalone/*.app Standalone/
