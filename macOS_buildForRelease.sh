d5352dc0ab2c69d5a84a8c312206affdb7b592d6
Felix Holzmüller <f.holzmueller@kug.ac.at>
2025-04-09 18:00:09 +0200

    Update build targets

--- macOS_buildForRelease.sh
+++ macOS_buildForRelease.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
 ## script for creating the release binaries on
-# rm -rf build
+rm -rf build
+
 rm -rf VST
 rm -rf VST3
 rm -rf LV2
 rm -rf AAX
 rm -rf Standalone
 
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DIEM_BUILD_VST2=ON -DVST2SDKPATH=src/VST_SDK/VST2_SDK/ -DIEM_BUILD_LV2=ON -DIEM_BUILD_STANDALONE=ON -DCMAKE_OSX_DEPLOYMENT_TARGET=10.11 -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64"
-cmake --build .
+cmake -B build -DCMAKE_BUILD_TYPE=Release -DIEM_BUILD_VST3=ON -DIEM_BUILD_VST2=ON -DVST2SDKPATH=src/VST_SDK/VST2_SDK/ -DIEM_BUILD_LV2=ON -DIEM_BUILD_STANDALONE=ON -DIEM_BUILD_AAX=ON -DCMAKE_OSX_DEPLOYMENT_TARGET=10.11 -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64"
+cmake --build build --config Release -- -j 8
 
 # cd ..
 mkdir VST
